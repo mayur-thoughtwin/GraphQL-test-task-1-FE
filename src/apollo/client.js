b4d2fc8cccb7075efa96a11cd6e342ce58e226b0
mayur-thoughtwin <mayur.panchal@thoughtwin.com>
2026-01-02 11:12:14 +0530

    Add env for backend

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
   uri: import.meta.env.VITE_BACKEND_URI,
-  uri: 'https://graphql-test-task-1-production.up.railway.app/graphql',
+  // uri: 'https://graphql-test-task-1-production.up.railway.app/graphql',
   // uri: 'http://localhost:4000/graphql',
 })
 
